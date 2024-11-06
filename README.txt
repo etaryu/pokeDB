912ab3670603725d29ae7284f2a540f02b4bd81b
etaryu <yuggoteruya1@gmail.com>
2024-11-06 09:48:37 -0300

    dump

---
+++ README.txt
@@ -0,0 +1,4 @@
+Nome dos Alunos: Shynji Miyazato, Yuggo Teruya
+Youtube Link Apresentação:
+Youtube Link Como instalar ambiente:                   
+
--- pokeBD.sql
+++ pokeBD.sql
@@ -18,3 +18,28 @@ CREATE TABLE habilidades (
     pokemon_id INTEGER REFERENCES pokemons(id),
     habilidade VARCHAR(100)
 );
+
+INSERT INTO pokemons (nome, identificacao, experiencia_base, altura, peso)
+VALUES
+    ('Pikachu', 25, 112, 4, 60), 
+    ('Charmander', 4, 62, 6, 85),  
+    ('Bulbasaur', 1, 64, 7, 69),  
+    ('Squirtle', 7, 63, 5, 90);    
+
+
+INSERT INTO tipos (pokemon_id, tipo)
+VALUES
+    (1, 'Elétrico'),  
+    (2, 'Fogo'),      
+    (3, 'Grama'),    
+    (3, 'Veneno'),    
+    (4, 'Água');      
+
+
+INSERT INTO habilidades (pokemon_id, habilidade)
+VALUES
+    (1, 'Static'),    
+    (1, 'Lightning Rod'),
+    (2, 'Blaze'),    
+    (3, 'Overgrow'),  
+    (4, 'Torrent');   
