a79fe9aea454ec589284554db9a23decba44569e
etaryu <yuggoteruya1@gmail.com>
2024-11-18 08:52:12 -0300

    pokeforca

---
+++ importPoke.js
@@ -0,0 +1,70 @@
+const { Client } = require('pg');
+const axios = require('axios');
+const readline = require('readline');
+
+const cliente = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'pokedb',
+  password: 'minhasenha',
+  port: 5433,
+});
+
+async function criarBanco() {
+  await cliente.connect();
+  console.log("Conectado ao banco de dados!");
+
+  // Criação das tabelas
+  const tabelaPokemon = `
+    CREATE TABLE IF NOT EXISTS pokemons (
+      id SERIAL PRIMARY KEY,
+      nome VARCHAR(50),
+      identificacao INT,
+      altura INT,
+      peso INT
+    );`;
+
+  const tabelaTipos = `
+    CREATE TABLE IF NOT EXISTS tipos (
+      id SERIAL PRIMARY KEY,
+      pokemon_id INT REFERENCES pokemons(id),
+      tipo VARCHAR(50)
+    );`;
+
+  await cliente.query(tabelaPokemon);
+  await cliente.query(tabelaTipos);
+  console.log("Tabelas criadas!");
+}
+
+async function importarPokemon(nomePokemon) {
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
+  const pokemon = response.data;
+
+  const pokemonSQL = 'INSERT INTO pokemons (nome, identificacao, altura, peso) VALUES ($1, $2, $3, $4) RETURNING id';
+  const valores = [pokemon.name, pokemon.id, pokemon.height, pokemon.weight];
+  const resultado = await cliente.query(pokemonSQL, valores);
+  const pokemonId = resultado.rows[0].id;
+
+  for (const tipo of pokemon.types) {
+    const tipoSQL = 'INSERT INTO tipos (pokemon_id, tipo) VALUES ($1, $2)';
+    const valoresTipo = [pokemonId, tipo.type.name];
+    await cliente.query(tipoSQL, valoresTipo);
+  }
+
+  console.log(`Pokémon ${pokemon.name} importado!`);
+}
+
+async function importarTodosPokemons() {
+  const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151`);
+  const pokemons = result.data.results;
+
+  for (const { name } of pokemons) {
+    await importarPokemon(name);
+  }
+  console.log("Todos os Pokémons da primeira geração foram importados!");
+}
+
+(async () => {
+    await criarBanco();
+    await importarTodosPokemons();
+})();
---
+++ pokeForca.js
@@ -0,0 +1,90 @@
+const { Client } = require('pg');
+const axios = require('axios');
+const readline = require('readline');
+
+// Configuração do cliente PostgreSQL
+const cliente = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'pokedb',
+  password: 'minhasenha',
+  port: 5433,
+});
+
+async function selecionarPokemonAleatorio() {
+  const resultado = await cliente.query('SELECT * FROM pokemons ORDER BY RANDOM() LIMIT 1');
+  return resultado.rows[0]; 
+}
+
+function iniciarJogo(pokemonAleatorio) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Vamos jogar! Descubra o nome do Pokémon no jogo de forca! Pronto? (sim/não): ', async (resposta) => {
+    if (resposta.toLowerCase() !== 'sim') {
+      console.log('Jogo encerrado. Até a próxima!');
+      rl.close();
+      return;
+    }
+
+    const pokemonData = pokemonAleatorio;
+    const tipos = await cliente.query('SELECT tipo FROM tipos WHERE pokemon_id = $1', [pokemonData.id]);
+
+    console.log(`Dica: Este Pokémon pesa ${pokemonData.peso} e tem ${pokemonData.altura} de altura.`);
+    console.log("Tipos:", tipos.rows.map((t) => t.tipo).join(', '));
+
+    let palavra = pokemonData.nome.toUpperCase();
+    let tentativas = 6;
+    let letrasAcertadas = Array(palavra.length).fill('_');
+
+    const verificarLetra = (letra) => {
+      let acertou = false;
+      for (let i = 0; i < palavra.length; i++) {
+        if (palavra[i] === letra) {
+          letrasAcertadas[i] = letra;
+          acertou = true;
+        }
+      }
+      return acertou;
+    };
+
+    const pedirLetra = () => {
+      console.log("Palavra:", letrasAcertadas.join(' '));
+      if (letrasAcertadas.join('') === palavra) {
+        console.log("Parabéns! Você acertou o nome do Pokémon!");
+        rl.close();
+        return;
+      }
+      if (tentativas <= 0) {
+        console.log("Você perdeu! O nome do Pokémon era:", palavra);
+        rl.close();
+        return;
+      }
+
+      rl.question("Digite uma letra: ", (letra) => {
+        letra = letra.toUpperCase();
+        if (letra.length !== 1) {
+          console.log("Digite apenas uma letra!");
+        } else if (verificarLetra(letra)) {
+          console.log("Você acertou uma letra!");
+        } else {
+          console.log("Letra errada!");
+          tentativas--;
+          console.log(`Tentativas restantes: ${tentativas}`);
+        }
+        pedirLetra();
+      });
+    };
+
+    pedirLetra();
+  });
+}
+
+
+(async () => {
+    await cliente.connect();
+    const pokemonAleatorio = await selecionarPokemonAleatorio(); 
+    iniciarJogo(pokemonAleatorio);
+})();
